0530f73971d7bdef1f4ebab66f09b23b7bfab203
AyukGiress <giress865@gmail.com>
2024-01-06 08:39:00 +0100

    my firsts

--- asset/styles/index.js
+++ asset/styles/index.js
@@ -1,11 +1,11 @@
 const progress = document.getElementById('progress')
-const next = document.getElementById('next')
 const prev = document.getElementById('prev')
+const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-next.addEventListener('click', () =>{
+next.addEventListener('click', () => {
     currentActive++
 
     if(currentActive > circles.length) {
@@ -15,3 +15,35 @@ next.addEventListener('click', () =>{
     update()
 })
 
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update () {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
